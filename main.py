c3e97a09722b8fb654125a36710137f096e80add
citlalli <csantiago28@toromail.csudh.edu>
2019-08-21 12:07:44 -0700

    added code

---
+++ main.py
@@ -0,0 +1,50 @@
+class Player (Character):
+    def __init__(self, _name, _level, _hp, _attack):
+        self.name = _name
+        self.level = _level
+        self.hp = _hp
+        self.attack = _attack
+        self.Dead = False
+    def attack(self):
+        if type(targetCharacter) == "Goblin":
+            if choice == 'a' or 'A'
+                self.hp = self.hp - self.attack
+
+
+
+
+class User (Player):
+        self.name = #user input
+        self. level = 1
+        self.hp = 200
+        self.attack =
+
+
+
+class Rooms (object): #possible ndb Model
+    def __init__(self,_room,_monsters):
+        self.room = _room
+        self.moster = _monsters
+        #ammount of monsters
+
+class Monster (Character):
+    def __init__(self, _damage, _level, _hp):
+        self.damage = _damage
+        self.level = _level
+        self.hp = _hp
+        self.Dead = False
+
+    def get_damage (self, amouDam)
+
+class Goblin(Monster):
+        self.hp = 100
+        self.level = 1
+        self.damage = 20
+class Hobgoblin(Monster):
+        self.hp = 150
+        self.level = 2
+        self.damage = 30
+class GoblinKing(Monster):
+        self.hp = 200
+        self.level = 3
+        self.damage = 50
